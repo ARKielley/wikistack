5a46fc72263fd397b99878151b71bfa50c83594f
Amanda Kielley <eatingfoil@gmail.com>
2018-09-21 14:19:34 -0400

    added HTML form

--- routes/wiki.js
+++ routes/wiki.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const main = require("..np/views/main.js");
+const main = require("../views/main.js");
 
 router.get("/", (req, res) => {
   res.send(main());
